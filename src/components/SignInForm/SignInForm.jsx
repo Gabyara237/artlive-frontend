7ab4ec7fab0225cd0838e9b16ff97a12b3828315
Gabriela Araujo <gabyara237@gmail.com>
2026-02-11 15:12:49 -0800

    feat: implement signup form with validation

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,6 +1,71 @@
+import { useState } from "react"
+import { useNavigate } from 'react-router'
+
+
+const initialData={
+    username : '',
+    password: "",
+}
+
 const SignInForm =() =>{
+    const navigate = useNavigate()
+
+    const [formData, setFormData] = useState(initialData);
+    const [message, setMessage] = useState("");
+
+    const {username, password} = formData;
+
+    const handleSubmit = (evt) =>{
+        evt.preventDefault()
+        try{
+            console.log(formData)
+        }catch(err){
+            setMessage(err.message)
+        }
+    }
+
+    const handleChange = (evt) =>{
+        setMessage("")
+        setFormData({...formData, [evt.target.name]:evt.target.value})
+    }
+
+    const isFormInvalid=()=>{
+        return !(username  && password)
+    }
+
     return(
-        <h1>Hi from SignIn</h1>
+        <main>
+            <h1>Sign In</h1>
+            {message && <p>{message}</p>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={username}
+                        onChange={handleChange}
+                    />
+
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                    />
+                
+                </div>
+                <div>
+                    <button disabled={isFormInvalid()}>Sign In</button>
+                    <button onClick={() => navigate('/')}>Cancel</button>
+                    
+                </div>
+            </form>
+
+        </main>
     )
 
 }
