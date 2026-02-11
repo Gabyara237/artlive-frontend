70c62e1bfaa2e57f668b24182848881f7f75eb5a
Gabriela Araujo <gabyara237@gmail.com>
2026-02-11 12:42:13 -0800

    feat: implement signup form with validation

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,11 +1,97 @@
+import { useState } from "react"
+import { useNavigate } from 'react-router'
 
+const initialData={
+    username : '',
+    email: "",
+    password: "",
+    passwordConfirm: ""
+}
 
 const SignUpForm =()=>{
+    const navigate = useNavigate()
+    const [message, setMessage] = useState("");
+
+    const [formData,setFormData] = useState(initialData)
+
+     const { username, email, password, passwordConfirm} = formData
+
+
+    const handleSubmit = async(evt) =>{
+        evt.preventDefault()
+        try{
+            console.log(formData)
+        }catch(err){
+            setMessage(err.message)
+        }
+
+    }
+
+    const handleChange =(evt)=>{
+        setFormData({...formData, [evt.target.name]: evt.target.value})
+    }
+
+    const isFormInvalid =()=>{
+        return !(username && email && password && password === passwordConfirm)
+    }
+
 
     return(
-        <h1>
-            Hi from SignUpForm
-        </h1>
+        <main>
+            <h1>Sign Up</h1>
+            {message && <p>{message}</p>}
+            <form onSubmit= {handleSubmit}>
+
+                <div>
+                    <label htmlFor="username"> Username:</label>
+                    <input 
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={username} 
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                        required
+                    />
+                    <label htmlFor="passwordConfirm">Confirm Password:</label>
+                    <input 
+                        type="password"
+                        id="passwordConfirm"
+                        name="passwordConfirm"
+                        value={passwordConfirm}
+                        onChange={handleChange}
+                        required
+                    />
+
+                </div>
+                <div>
+                    <button disabled={isFormInvalid()}>Sign Up</button>
+                    <button onClick={() => navigate('/')}>Cancel</button>
+                </div>
+
+            </form>
+
+
+        </main>
     )
 
 }
