613e540d65e1c55ea0e2b4bb4bc17a2a693a999d
Gabriela Araujo <gabyara237@gmail.com>
2026-02-11 16:12:53 -0800

    feat: add WorkshopList component and conditionally render home based on auth state

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 
+import { useContext } from 'react';
 import { Route, Routes } from 'react-router';
 import './App.css'
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import NavBar from './components/NavBar/NavBar';
+import WorkshopList from './components/WorkshopList/WorkshopList';
+import { UserContext } from './contexts/UserContext';
+
 
 
 const App = () => {
 
+  const {user} = useContext(UserContext)
 
   return(
     <>
       <NavBar/>
       <Routes>
-        <Route path={'/'} element ={<h1>Hello world!</h1>}></Route>
+        <Route path={'/'} element ={user?<WorkshopList/> :<h1>Hello world!</h1>}></Route>
         <Route path={'/sign-up'} element={<SignUpForm/>}></Route>
         <Route path={'/sign-in'} element={<SignInForm/>}></Route>
 
---
+++ src/components/WorkshopList/WorkshopList.jsx
@@ -0,0 +1,8 @@
+const WorkshopList = () =>{
+    return(
+        <h1>Hello from WorkshopList</h1>
+    )
+
+}
+
+export default WorkshopList
