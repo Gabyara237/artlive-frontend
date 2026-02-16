2b4371f47f076826eecc820906e31642bd090c3c
Gabriela Araujo <gabyara237@gmail.com>
2026-02-16 15:37:07 -0800

    feat: create reusable SVG icon components for landing features

---
+++ src/components/icons/IconExperience.jsx
@@ -0,0 +1,32 @@
+const IconExperience =({className}) =>{
+    return(
+        <svg className={className} viewBox="0 0 500 500" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_475_140" maskUnits="userSpaceOnUse" x="0" y="0" width="500" height="500">
+                <path d="M0 0H500V500H0V0Z" fill="white"/>
+            </mask>
+            <g mask="url(#mask0_475_140)">
+                <path d="M9.76562 68.3594C42.1289 68.3594 68.3594 42.1289 68.3594 9.76562C68.3594 25.9473 74.9219 40.5957 85.5176 51.2012C96.123 61.7969 110.771 68.3594 126.953 68.3594M9.76562 68.3594C42.1289 68.3594 68.3594 94.5898 68.3594 126.953C68.3594 110.771 74.9219 96.123 85.5176 85.5176C96.123 74.9219 110.771 68.3594 126.953 68.3594" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M373.047 68.3594C405.41 68.3594 431.641 42.1289 431.641 9.76562C431.641 25.9473 438.203 40.5957 448.799 51.2012C459.404 61.7969 474.053 68.3594 490.234 68.3594M373.047 68.3594C405.41 68.3594 431.641 94.5898 431.641 126.953C431.641 110.771 438.203 96.123 448.799 85.5176C459.404 74.9219 474.053 68.3594 490.234 68.3594" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M373.047 431.641C405.41 431.641 431.641 405.41 431.641 373.047C431.641 389.229 438.203 403.877 448.799 414.482C459.404 425.078 474.053 431.641 490.234 431.641M373.047 431.641C405.41 431.641 431.641 457.871 431.641 490.234C431.641 474.053 438.203 459.404 448.799 448.799C459.404 438.203 474.053 431.641 490.234 431.641" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M9.76562 431.641C42.1289 431.641 68.3594 405.41 68.3594 373.047C68.3594 389.229 74.9219 403.877 85.5176 414.482C96.123 425.078 110.771 431.641 126.953 431.641M9.76562 431.641C42.1289 431.641 68.3594 457.871 68.3594 490.234C68.3594 474.053 74.9219 459.404 85.5176 448.799C96.123 438.203 110.771 431.641 126.953 431.641" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M367.188 250C302.471 250 250 302.471 250 367.188" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M132.812 250C197.529 250 250 302.471 250 367.188" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M367.188 249.998C302.471 249.998 250 197.527 250 132.811" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M250.781 48.8281V9.76562" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M270.312 29.2969H231.25" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M30.0781 269.529V230.467" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M49.6094 249.998H10.5469" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M250 490.234V451.172" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M269.531 470.703H230.469" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M470.703 269.529V230.467" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M490.234 249.998H451.172" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M210.938 227.467C214.673 227.467 217.703 224.437 217.703 220.701C217.703 216.965 214.673 213.936 210.938 213.936C207.202 213.936 204.172 216.965 204.172 220.701C204.172 224.437 207.202 227.467 210.938 227.467Z" fill="currentColor" stroke="currentColor" strokeWidth="6"/>
+                <path d="M132.812 249.998C146.941 249.998 160.487 247.497 173.03 242.914" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M237.432 185.664C245.47 169.784 249.999 151.828 249.999 132.813" stroke="currentColor" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            </g>
+        </svg> 
+    )
+
+}
+
+export default IconExperience
---
+++ src/components/icons/IconInstructor.jsx
@@ -0,0 +1,27 @@
+const IconInstructor = ({className}) =>{
+    return(
+        <svg className={className} viewBox="0 0 405 525" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M337.304 212.795C337.304 287.244 276.95 347.598 202.501 347.598C128.051 347.598 67.6973 287.244 67.6973 212.795C67.6973 138.345 128.051 77.9914 202.501 77.9914C276.95 77.9914 337.304 138.345 337.304 212.795Z" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M202.5 2.50024L257.487 44.6406L326.107 43.7412L346.457 109.281L402.5 148.887L380.44 213.871L402.5 278.854L346.457 318.461L326.107 384.001L257.487 383.101L202.5 424.163L147.513 383.101L78.8933 384.001L58.5435 318.461L2.5 278.854L24.5602 213.871L2.5 148.887L58.5435 109.281L78.8933 43.7412L147.513 44.6406L202.5 2.50024Z" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M176.668 404.873L121.619 522.3L91.5878 471.221L33.8857 479.712L79.1709 383.998" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M228.184 404.984L283.18 522.3L313.21 471.221L370.914 479.712L325.626 383.995" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <mask id="mask0_476_246" maskUnits="userSpaceOnUse" x="125" y="131" width="163" height="163">
+                <path d="M285.196 291.176V134.412H128.433V291.176H285.196Z" fill="white" stroke="white" strokeWidth="5"/>
+            </mask>
+            <g mask="url(#mask0_476_246)">
+                <path d="M264.597 230.932C260.329 264.618 231.568 290.668 196.723 290.668C158.933 290.668 128.302 260.034 128.302 222.247C128.302 209.883 131.582 198.234 137.319 188.166M143.662 178.883C150.659 170.244 159.698 163.341 170.038 158.957C170.038 158.957 175.368 156.556 180.644 156.562C185.804 156.572 190.907 158.887 190.907 167.51C190.907 167.51 190.221 185.3 209.038 199.67C209.038 199.67 222.036 209.931 245.299 209.931C246.197 209.931 247.385 209.9 248.737 209.919M169.354 199.326C161.797 199.326 155.67 194.119 155.67 187.695C155.67 181.27 161.797 176.063 169.354 176.063C176.911 176.063 183.038 181.27 183.038 187.695C183.038 194.119 176.911 199.326 169.354 199.326Z" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M161.143 218.142C161.143 224.566 155.936 229.773 149.512 229.773C143.088 229.773 137.88 224.566 137.88 218.142C137.88 211.718 143.088 206.511 149.512 206.511C155.936 206.511 161.143 211.718 161.143 218.142Z" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M178.933 250.642C178.933 257.066 173.726 262.273 167.302 262.273C160.878 262.273 155.67 257.066 155.67 250.642C155.67 244.218 160.878 239.011 167.302 239.011C173.726 239.011 178.933 244.218 178.933 250.642Z" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M212.459 263.642C212.459 270.066 207.252 275.273 200.828 275.273C194.404 275.273 189.196 270.066 189.196 263.642C189.196 257.218 194.404 252.011 200.828 252.011C207.252 252.011 212.459 257.218 212.459 263.642Z" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M246.327 246.877C246.327 253.301 241.12 258.509 234.696 258.509C228.272 258.509 223.064 253.301 223.064 246.877C223.064 240.453 228.272 235.246 234.696 235.246C241.12 235.246 246.327 240.453 246.327 246.877Z" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M234.083 190.507L275.188 244.964C277.014 247.404 280.45 247.949 282.941 246.192C285.431 244.436 286.072 241.017 284.386 238.478L253.669 191.781" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M247.508 182.411L234.726 162.98L227.736 167.909L220.747 172.838L227.397 181.648" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M228.811 154.594C228.811 154.594 229.635 149.827 225.298 143.676C220.171 136.408 209.995 135.63 209.995 135.63L204.471 134.921L205.956 137.026C207.241 138.847 205.345 141.216 203.285 140.362C203.285 140.362 200.601 150.208 205.727 157.478C210.065 163.628 214.832 164.452 214.832 164.452L228.811 154.594Z" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M220.747 172.84L214.832 164.452" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M228.812 154.594L234.726 162.981" stroke="currentColor" strokeWidth="5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            </g>
+        </svg>
+    )
+}
+
+export default IconInstructor
