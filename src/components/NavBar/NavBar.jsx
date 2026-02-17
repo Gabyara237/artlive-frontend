31ce3043b61c5558be94101aa3c0b1c856be1a26
Gabriela Araujo <gabyara237@gmail.com>
2026-02-17 00:55:49 -0800

    feat: enhance navbar with responsive layout and improved styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,52 +1,59 @@
-import { useContext } from 'react'
-import { Link } from "react-router"
-
+import { useContext, useState } from 'react'
+import { Link, NavLink } from "react-router" 
 import './NavBar.css'
-
 import { UserContext } from '../../contexts/UserContext'
 
 const NavBar = () =>{
     const { user, setUser } = useContext(UserContext)
+    const [isOpen, setIsOpen] = useState(false);
 
     const handleSignOut = () => {
         localStorage.removeItem('token')
         setUser(null)
+        setIsOpen(false);
     }
+
+    const getActiveClassName = ({ isActive }) => isActive ? "nav-link active" : "nav-link";
 
     return(
         <nav className='nav-bar'>
             <div>
-                <Link to='/'><h1 className='logo'>ArtLive</h1></Link>
+                <Link to='/' onClick={() => setIsOpen(false)}>
+                    <h1 className='logo'>ArtLive</h1>
+                </Link>
             </div>
-            {user?(
-                <>
-                    <Link to='/'> Home </Link>
-                    <Link to='/workshops'>Workshops</Link>
 
-                    {user?.role === "instructor" && (
-                        <>
-                            <Link to="/workshops/new"> Create Workshop </Link>
-                            <Link to="/users/me/workshops"> My Workshops</Link>
-                        </>
-                    )}
-                    {user?.role === "student" && (
+            <button className="nav-toggle" onClick={() => setIsOpen(!isOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
 
-                        <Link to="/users/me/registrations"> My Workshops </Link>
+            <div className={`nav-links ${isOpen? 'is-open':''}`}>
+                {user?(
+                    <>
+                        <NavLink className={getActiveClassName} to='/' onClick={() => setIsOpen(false)}> Home </NavLink>
+                        <NavLink className={getActiveClassName} end to='/workshops' onClick={() => setIsOpen(false)}>Workshops</NavLink>
 
-                    )}
-                    <Link to='/' onClick={handleSignOut}> Sign Out </Link>
-                
-                </>
-            ):(
-                <>
-                    <div className='navbar-actions'>
-                        <Link to='/sign-in'> Sign In </Link>
-                        <Link to='/sign-up' className='highlight'> Sign Up </Link>
-                    </div>
-                </>
+                        {user?.role === "instructor" && (
+                            <>
+                                <NavLink className={getActiveClassName} to="/workshops/new" onClick={() => setIsOpen(false)}> Create Workshop </NavLink>
+                                <NavLink className={getActiveClassName} to="/users/me/workshops" onClick={() => setIsOpen(false)}> My Workshops</NavLink>
+                            </>
+                        )}
+                        {user?.role === "student" && (
+                            <NavLink className={getActiveClassName} to="/users/me/registrations" onClick={() => setIsOpen(false)}> My Workshops </NavLink>
+                        )}
             
-            )}
-
+                        <Link className="nav-link" to='/' onClick={handleSignOut}> Sign Out </Link>
+                    </>
+                ):(
+                    <div className='navbar-actions'>
+                        <NavLink className={getActiveClassName} to='/sign-in' onClick={() => setIsOpen(false)}> Sign In </NavLink>
+                        <NavLink className={({ isActive }) => isActive ? "highlight active" : "highlight" } to='/sign-up' onClick={() => setIsOpen(false)}> Sign Up </NavLink>
+                    </div>
+                )}
+            </div>
         </nav>
     )
 }
